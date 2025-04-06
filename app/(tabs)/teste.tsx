023e8d916614ca830d3ee6ca219739b4c84770e3
Gabriel Alves <gabhenriquesa1@gmail.com>
2025-04-06 18:22:11 -0300

    refactor: remove default React Native components
    Co-authored-by: Tiago Figueiredo Reis <tiagoreistp@hotmail.com>
    Co-authored-by: CaioMendesRRosa <CaioMendesRRosa@users.noreply.github.com>
    Co-authored-by: Davi <davi2907@users.noreply.github.com>

--- app/(tabs)/teste.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-
-export default function Teste(){
-    return (
-        <Text className="text-black m-auto capitalize" >olá react-native com tailwind</Text>
-    )
-}
