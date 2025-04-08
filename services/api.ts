e5a606da68a85152505e231456926b623bdabc3c
Gabriel Alves <gabhenriquesa1@gmail.com>
2025-04-07 21:48:52 -0300

    feat: add now play movies request
    Co-authored-by: CaioMendesRRosa <CaioMendesRRosa@users.noreply.github.com>
    Co-authored-by: Davi <davi2907@users.noreply.github.com>
    Co-authored-by: Tiago Figueiredo Reis <tiagoreistp@hotmail.com>

--- services/api.ts
+++ services/api.ts
@@ -1,9 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
+import token from "../token.json";
 
 export const api = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/search/', // substitua pela URL da sua API
+  baseURL: "https://api.themoviedb.org/", // substitua pela URL da sua API
   timeout: 10000, // opcional: tempo limite de requisição
 });
 
-
-
+export const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: token.Authorization,
+  },
+};
--- services/searchContent.ts
+++ services/searchContent.ts
@@ -5,26 +5,34 @@ import React from "react";
 
 export const requestContents = async (
   mediaSearch: string,
-  setMedias: React.Dispatch<React.SetStateAction<MovieSearchProps[]>>
+  setMedias: React.Dispatch<React.SetStateAction<MovieSearchProps[]>>,
 ) => {
   const defaultUrl = `query=${mediaSearch}&include_adult=false&language=pt-BR&page=1`;
   try {
-    const resp = await api.get(
-      `movie?${defaultUrl}`,
-      options
-    );
-    const resp2 = await api.get(
-      `tv?${defaultUrl}`,
-      options
-    );
+    const resp = await api.get(`3/search/movie?${defaultUrl}`, options);
+    const resp2 = await api.get(`3/search/tv?${defaultUrl}`, options);
 
-    for (let i in resp2.data.results)
-    {
-        resp2.data.results[i]['title'] = resp2.data.results[i]['name'];
-        delete resp2.data.results[i]['nome'];
+    for (let i in resp2.data.results) {
+      resp2.data.results[i]["title"] = resp2.data.results[i]["name"];
+      delete resp2.data.results[i]["nome"];
     }
 
     setMedias([...resp.data.results, ...resp2.data.results]);
+  } catch (error) {
+    console.error("Erro ao buscar filmes:", error);
+  }
+};
+
+export const initialRequest = async (
+  setMedias: React.Dispatch<React.SetStateAction<MovieSearchProps[]>>,
+) => {
+  try {
+    const NowPlaying = await api.get(
+      "3/movie/now_playing?language=pt-br&page=1",
+      options,
+    );
+    console.log(NowPlaying);
+    setMedias([...NowPlaying.data.results]);
   } catch (error) {
     console.error("Erro ao buscar filmes:", error);
   }
