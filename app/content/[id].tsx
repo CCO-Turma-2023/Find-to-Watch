47337e29f0f6daab6e62aac0a1a86a19aebc9b33
Gabriel Alves <gabhenriquesa1@gmail.com>
2025-04-12 14:53:35 -0300

    feat: customized the content description page with an improved layout.

--- app/content/[id].tsx
+++ app/content/[id].tsx
@@ -1,14 +1,18 @@
-import { View, Text, Pressable } from 'react-native';
-import { useEffect, useState } from 'react';
-import { router, useLocalSearchParams } from 'expo-router';
-import {getFilmId} from "@/services/scrap";
+import { View, Text, Pressable, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { getFilmId } from "@/services/scrap";
 import { useContextHome } from "@/contexts/ContextHome";
 import { RequestMediabyId } from "@/services/searchContent";
 import { MovieSearchProps } from "@/interfaces/search-interface";
+import { Ionicons } from "@expo/vector-icons";
+// Removed unused import for MaterialIcons
 
 export default function Filmes() {
   const { id } = useLocalSearchParams();
-  const [currentMovie, setCurrentMovie] = useState<MovieSearchProps | null>(null);
+  const [currentMovie, setCurrentMovie] = useState<MovieSearchProps | null>(
+    null,
+  );
   const [nowPlaying, setNowPlaying] = useState(false);
   const { media } = useContextHome();
 
@@ -35,35 +39,56 @@ export default function Filmes() {
         foundMovie = await RequestMediabyId(id);
       }
 
-      if(id[id.length - 1] === "1"){
-        foundMovie["movie"] = true
-      }else{
-        foundMovie["title"] = foundMovie["name"]
+      if (id[id.length - 1] === "1") {
+        foundMovie["movie"] = true;
+      } else {
+        foundMovie["title"] = foundMovie["name"];
         delete foundMovie["name"];
-        foundMovie["movie"] = false
+        foundMovie["movie"] = false;
       }
 
       setCurrentMovie(foundMovie);
     };
 
     findContent();
-
   }, [media]);
 
   return (
-    <View className="bg-black flex-1">
+    <View className="flex flex-1 gap-3 bg-black p-2">
+      <Pressable onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={30} color="#ffffff" />
+      </Pressable>
+
+      <Image
+        source={{
+          uri: `https://image.tmdb.org/t/p/original${currentMovie?.poster_path}`,
+        }}
+        style={{
+          width: "100%",
+          height: 300,
+          resizeMode: "contain", // Ajusta a imagem mantendo a proporção sem cortar
+        }}
+      />
+
       {media && media.length > 0 && (
-        <>
+        <View className="flex flex-col gap-4">
           <Text className="text-white">
-            Informações de <Text className="font-bold">{currentMovie?.title}</Text> aparecerão aqui
+            <Text className="font-bold">{currentMovie?.title}</Text>
           </Text>
-  
+
+          <Text className="text-zinc-300">
+            {currentMovie?.overview && currentMovie?.overview?.length > 0
+              ? currentMovie.overview
+              : "Sem sinopse"}
+          </Text>
           {nowPlaying && (
-            <Pressable onPress={OnPress}>
-              <Text className="text-white">Ver Sessões</Text>
+            <Pressable className="flex w-full items-center" onPress={OnPress}>
+              <Ionicons name="film" size={30} color="#ffffff" />
+
+              <Text className="text-white">Sessões Disponíveis</Text>
             </Pressable>
           )}
-        </>
+        </View>
       )}
     </View>
   );
