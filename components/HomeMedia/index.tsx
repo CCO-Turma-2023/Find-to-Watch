87db351254d6c8d2507afc2767098932decdfb2e
tiago <tiagofigreis@gmail.com>
2025-04-13 13:26:57 -0300

    feat: -changes in the way of requesting the tmdb api -changes in the way of rendering components in the initial tabs

--- components/HomeMedia/index.tsx
+++ components/HomeMedia/index.tsx
@@ -1,92 +1,74 @@
-import ShowMedia from "@/components/AllShowMedia";
 import { useContextHome } from "@/contexts/ContextHome";
-import { ScrollView, Text, View } from "react-native";
+import { FlatList, Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+import { MovieSearchProps } from "@/interfaces/search-interface";
+
+const categorias = [
+  { titulo: "Em Cartaz", index: 0 },
+  { titulo: "Tendências", index: 1 },
+  { titulo: "Ação", index: 2 },
+  { titulo: "Drama", index: 3 },
+  { titulo: "Comédia", index: 4 },
+  { titulo: "Animação", index: 5 },
+  { titulo: "Documentário", index: 6 },
+  { titulo: "Terror", index: 7 },
+  { titulo: "Romance", index: 8 },
+  { titulo: "Ficção Científica", index: 9 },
+  { titulo: "Musical", index: 10 },
+  { titulo: "História", index: 11 },
+  { titulo: "Suspense", index: 12 },
+];
 
 export default function Home() {
   const { media } = useContextHome();
+
   return (
-    <ScrollView
+    <FlatList
+      data={categorias}
+      keyExtractor={(item) => item.titulo}
       contentContainerStyle={{ paddingBottom: 32 }}
       showsVerticalScrollIndicator={false}
       className="bg-black py-2"
-    >
-      {/* Seção: Em Cartaz */}
-      <View className="mb-4">
-        <Text className="pl-2 text-lg font-semibold text-white">Em Cartaz</Text>
-        <ShowMedia medias={media[0]} horizontal={true} />
-      </View>
+      renderItem={({ item }) => {
+        const categoriaFilmes: MovieSearchProps[] = media[item.index]?.slice(0, 10) || []; // Limita a 10 itens
 
-      {/* Seção: Tendências */}
-      <View className="mb-4">
-        <Text className="pl-2 text-lg font-semibold text-white">Tendências</Text>
-        <ShowMedia medias={media[1]} horizontal={true} />
-      </View>
+        return (
+          <View className="mb-8">
+            <Text className="pl-2 text-lg font-semibold text-white">
+              {item.titulo}
+            </Text>
 
-      {/* Seção: Ação */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Ação</Text>
-        <ShowMedia medias={media[2]} horizontal={true} />
-      </View>
-
-      {/* Seção: Drama */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Drama</Text>
-        <ShowMedia medias={media[3]} horizontal={true} />
-      </View>
-
-      {/* Seção: Comédia */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Comédia</Text>
-        <ShowMedia medias={media[4]} horizontal={true} />
-      </View>
-
-      {/* Seção: Animação */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Animação</Text>
-        <ShowMedia medias={media[5]} horizontal={true} />
-      </View>
-
-      {/* Seção: Documentário */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Documentário</Text>
-        <ShowMedia medias={media[6]} horizontal={true} />
-      </View>
-
-      {/* Seção: Terror */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Terror</Text>
-        <ShowMedia medias={media[7]} horizontal={true} />
-      </View>
-
-      {/* Seção: Romance */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Romance</Text>
-        <ShowMedia medias={media[8]} horizontal={true} />
-      </View>
-
-      {/* Seção: Ficção Científica */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Ficção Científica</Text>
-        <ShowMedia medias={media[9]} horizontal={true} />
-      </View>
-
-      {/* Seção: Musical */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Musical</Text>
-        <ShowMedia medias={media[10]} horizontal={true} />
-      </View>
-
-      {/* Seção: História */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">História</Text>
-        <ShowMedia medias={media[11]} horizontal={true} />
-      </View>
-
-      {/* Seção: Suspense */}
-      <View>
-        <Text className="pl-2 text-lg font-semibold text-white">Suspense</Text>
-        <ShowMedia medias={media[12]} horizontal={true} />
-      </View>
-    </ScrollView>
+            <FlatList
+              data={categoriaFilmes}
+              keyExtractor={(mediaItem) => mediaItem.id.toString()}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 8, paddingTop: 8 }}
+              renderItem={({ item: mediaItem }) => (
+                <View className="mr-2">
+                  <Link
+                    href={{
+                      pathname: "/content/[id]",
+                      params: {
+                        id: `${mediaItem.id}${mediaItem.hasOwnProperty("title") ? 1 : 0}`,
+                      },
+                    }}
+                  >
+                    {mediaItem.poster_path && (
+                      <Image
+                        source={{
+                          uri: `https://image.tmdb.org/t/p/original${mediaItem.poster_path}`,
+                        }}
+                        className="h-60 w-44 rounded"
+                      />
+                    )}
+                  </Link>
+                </View>
+              )}
+            />
+          </View>
+        );
+      }}
+    />
   );
 }
