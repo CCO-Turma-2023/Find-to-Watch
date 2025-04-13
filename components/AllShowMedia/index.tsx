dd001e03f29970c4112f7d2fa693363ebc685960
tiago <tiagofigreis@gmail.com>
2025-04-13 13:38:46 -0300

    fix: return AllShowMedia component

---
+++ components/AllShowMedia/index.tsx
@@ -0,0 +1,46 @@
+import {
+  View,
+  Image,
+  ScrollView,
+} from "react-native";
+
+import { Link } from "expo-router";
+import { MovieSearchProps } from "@/interfaces/search-interface";
+
+interface ShowMediaProps {
+  medias: MovieSearchProps[];
+  horizontal?: boolean;
+}
+
+export default function ShowMedia({ medias, horizontal }: ShowMediaProps) {
+  return (
+    <ScrollView
+      horizontal={horizontal}
+      showsHorizontalScrollIndicator={!horizontal}
+      className="px-2 pt-2"
+    >
+      {medias?.length > 0 &&
+        medias.map((media, index) => (
+          <View key={index} className="mb-2 flex p-1">
+            <Link
+              href={{
+                pathname: "/content/[id]",
+                params: {
+                  id: `${media.id}${media.hasOwnProperty("title") ? 1 : 0}`,
+                },
+              }}
+            >
+              {media.poster_path && (
+                <Image
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/original${media.poster_path}`,
+                  }}
+                  className="h-36 w-24"
+                />
+              )}
+            </Link>
+          </View>
+        ))}
+    </ScrollView>
+  );
+}
