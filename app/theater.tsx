9ffe08c2022e48eb79dd76143bb909349359f6d4
Gabriel Alves <gabhenriquesa1@gmail.com>
2025-05-12 15:27:37 -0300

    style: Increased the size of the map

--- app/theater.tsx
+++ app/theater.tsx
@@ -1,8 +1,16 @@
-import { View, Text, TextInput, Keyboard, TouchableOpacity, ScrollView, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Keyboard,
+  TouchableOpacity,
+  ScrollView,
+  Pressable,
+} from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { PROVIDER_GOOGLE } from "react-native-maps"
-import React, { useEffect, useState, useRef } from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useEffect, useState, useRef } from "react";
+import MapView, { Marker } from "react-native-maps";
 import { getTheaterInfo } from "@/services/scrap";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Ionicons from "@expo/vector-icons/build/Ionicons";
@@ -11,96 +19,98 @@ import { TheaterInterface } from "@/interfaces/theater-interface";
 import { useContextCinema } from "@/contexts/ContextCinema";
 import { useLocation } from "@/contexts/ContextLocation";
 
+export default function Theater() {
+  const { region, theaters, subregion, setTheaters } = useLocation();
+  const [showMap, setShowMap] = useState(true);
+  const [busca, setBusca] = useState("");
+  const { setCine } = useContextCinema();
 
-export default function Theater(){
-        const { region, theaters, subregion, setTheaters } = useLocation(); 
-        const [showMap, setShowMap] = useState(true)
-        const [busca, setBusca] = useState("");
-        const { setCine } = useContextCinema();
+  const buscarCinemas = async () => {
+    setShowMap(false);
+    try {
+      if (!busca.trim()) return;
+      const theatersInfo = await getTheaterInfo(busca.trim());
+      setTheaters(theatersInfo);
 
-        const buscarCinemas = async () => {
-            setShowMap(false)
-            try {
-            if (!busca.trim()) return;
-            const theatersInfo = await getTheaterInfo(busca.trim());
-            setTheaters(theatersInfo);
-            Keyboard.dismiss();
-            } catch (error) {
-            console.error("Erro na busca:", error);
-            }
-        };
+      console.log("Theaters:", JSON.stringify(theatersInfo, null, 2));
 
-        const onPress = (cinema : TheaterInterface) => {
-            setCine(cinema);
-            router.dismiss();
-        }
+      Keyboard.dismiss();
+    } catch (error) {
+      console.error("Erro na busca:", error);
+    }
+  };
 
-        useEffect(() => {
-            (async () => {
-                if (busca.trim() === "") {
-                    setShowMap(true);
-                    const theatersInfo = await getTheaterInfo(subregion);
-                    setTheaters(theatersInfo);
-                }
-            })();
-        }, [busca]);
+  const onPress = (cinema: TheaterInterface) => {
+    setCine(cinema);
+    router.dismiss();
+  };
 
+  useEffect(() => {
+    (async () => {
+      if (busca.trim() === "") {
+        setShowMap(true);
+        const theatersInfo = await getTheaterInfo(subregion);
+        setTheaters(theatersInfo);
+      }
+    })();
+  }, [busca]);
 
-    if (!region) return null;
+  if (!region) return null;
 
-    return (
-        <View className="flex flex-1 flex-col bg-black p-2">
-            <StatusBar style="light" backgroundColor="black" translucent={false} />
+  return (
+    <View className="flex flex-1 flex-col bg-black p-2">
+      <StatusBar style="light" backgroundColor="black" translucent={false} />
 
-            <Pressable className="mb-4" onPress={() => router.back()}>
-                <Ionicons name="arrow-back" size={30} color="#ffffff" />
-            </Pressable>
-            <View className="mb-4 flex flex-row items-center gap-2 rounded border border-gray-400 p-2">
-                <AntDesign name="search1" color={"white"} size={20} />
-                <TextInput
-                className="ml-2 flex-1 text-white"
-                placeholder="Busque uma cidade"
-                placeholderTextColor="gray"
-                value={busca}
-                onChangeText={setBusca}
-                onSubmitEditing={buscarCinemas} // Pressionar "OK" no teclado
-                returnKeyType="search"
-                />
-                <TouchableOpacity onPress={buscarCinemas}>
-                    <AntDesign name="arrowright" size={24} color="white" />
-                </TouchableOpacity>
-            </View>
-            <View className="flex flex-1 flex-col items-center">
-                {showMap && (
-                    <MapView 
-                    style={{ width: '50%', height: '50%' }} 
-                    provider={PROVIDER_GOOGLE}
-                    region={region}
-                    >
-                        <Marker coordinate={region} />
-                    </MapView>)
-                }
-                    
-          
-            {theaters && (
-            <ScrollView className="mt-4 w-full" showsVerticalScrollIndicator={false}>
-                {theaters.map((t, index) => (
-                <View key={index} className="mb-4">
-                    <Pressable className="rounded-xl border border-white p-2" onPress={() => onPress(t)} >
-                        <Text className="text-white text-lg font-semibold">
-                        {t.cinema}
-                        </Text>
-                        <Text className="text-white text-sm">
-                        {t.endereco}
-                        </Text>
-                    </Pressable>
-                </View>
-               
-                ))}
-            </ScrollView>
-            )}
-            </View>
-        </View>
-    )
+      <Pressable className="mb-4" onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={30} color="#ffffff" />
+      </Pressable>
+      <View className="mb-4 flex flex-row items-center gap-2 rounded border border-gray-400 p-2">
+        <AntDesign name="search1" color={"white"} size={20} />
+        <TextInput
+          className="ml-2 flex-1 text-white"
+          placeholder="Busque uma cidade"
+          placeholderTextColor="gray"
+          value={busca}
+          onChangeText={setBusca}
+          onSubmitEditing={buscarCinemas} // Pressionar "OK" no teclado
+          returnKeyType="search"
+        />
+        <TouchableOpacity onPress={buscarCinemas}>
+          <AntDesign name="arrowright" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+      <View className="flex flex-1 flex-col items-center">
+        {showMap && (
+          <MapView
+            style={{ width: "100%", height: "100%" }}
+            provider={PROVIDER_GOOGLE}
+            region={region}
+          >
+            <Marker coordinate={region} />
+          </MapView>
+        )}
 
+        {theaters && (
+          <ScrollView
+            className="mt-4 w-full"
+            showsVerticalScrollIndicator={false}
+          >
+            {theaters.map((t, index) => (
+              <View key={index} className="mb-4">
+                <Pressable
+                  className="rounded-xl border border-white p-2"
+                  onPress={() => onPress(t)}
+                >
+                  <Text className="text-lg font-semibold text-white">
+                    {t.cinema}
+                  </Text>
+                  <Text className="text-sm text-white">{t.endereco}</Text>
+                </Pressable>
+              </View>
+            ))}
+          </ScrollView>
+        )}
+      </View>
+    </View>
+  );
 }
